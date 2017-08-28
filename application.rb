793986be95ba3e63d12f3b3d8e62586ea340ea53
Marlon Merjos <marlonmerjos@gmail.com>
2017-08-28 09:57:38 -0400

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,13 @@
 class Application
 
+  attr_accessor :name
+
+  @@name = "Marlon"
+
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is #{@@name}"
     resp.finish
   end
 
 end
-
